c6a79985e71b62b13807e1aeab52d81a2dc5201c
ifan0507 <ipan.lmj0507@gmail.com>
2025-03-03 11:10:41 +0700

    update verifikasi

--- resources/views/siswa/dashboard.blade.php
+++ resources/views/siswa/dashboard.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.siswa.template')
 @section('content')
-    <h1>Selamat datang {{ $data->siswa->nama }}</h1>
+    <div class="tab-content" id="formulir-tabsContent">
+        <div class="tab-pane fade show active" id="biodata" role="tabpanel" aria-labelledby="biodata-tab"></div>
+        <div class="tab-pane fade" id="keluarga" role="tabpanel" aria-labelledby="keluarga-tab"></div>
+        <!-- ekonomi, rumah, aset -->
+        <div class="tab-pane fade" id="ekonomi" role="tabpanel" aria-labelledby="ekonomi-tab"></div>
+        <div class="tab-pane fade" id="rumah" role="tabpanel" aria-labelledby="rumah-tab"></div>
+        <div class="tab-pane fade" id="aset" role="tabpanel" aria-labelledby="aset-tab"></div>
+        <div class="tab-pane fade" id="prestasi" role="tabpanel" aria-labelledby="prestasi-tab"></div>
+        <div class="tab-pane fade" id="rencana" role="tabpanel" aria-labelledby="rencana-tab"></div>
+        <div class="tab-pane fade" id="seleksi" role="tabpanel" aria-labelledby="seleksi-tab"></div>
+    </div>
 @endsection
